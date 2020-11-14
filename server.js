421b95f79dcbe0172e51ff32039003316b2f55ac
jananathj <jananathj@pickme.lk>
2020-11-14 16:10:02 +0530

    test

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if(cert_file) {
   }
 }
 
-var etcdHost = process.env.ETCD_HOST || '172.17.42.1';
+var etcdHost = process.env.ETCD_HOST || '35.226.128.15';
 var etcdPort = process.env.ETCD_PORT || 4001;
 var serverPort = process.env.SERVER_PORT || 8000;
 var publicDir = 'frontend';
